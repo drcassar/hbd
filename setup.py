6a7dbf0269a60e974b79def8bd37a1225fa1fdbe
Daniel R. Cassar <daniel.r.cassar@gmail.com>
2020-03-19 10:32:26 -0300

    bumped ver.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='hbd',
-    version='1.6.2',
+    version='1.6.3',
     author='Daniel Roberto Cassar',
     author_email='daniel.r.cassar@gmail.com',
     description='hbd',
